b2289ea125d06ce14bbb4505272105f38ef2c893
trws2 <dchen@cs.umd.edu>
2025-03-30 11:04:56 +0800

    update

--- day37/76_minimum_window_substring.py
+++ day37/76_minimum_window_substring.py
@@ -35,3 +35,34 @@ class Solution:
                 start_index += 1
 
         return "" if min_window[1] > len(s) else s[min_window[0]:min_window[1]+1]
+
+
+# solution passing simple test cases but running out of time on long s and t
+
+from collections import Counter
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        def s_contains_t(s, t):
+            for c, f in t.items():
+                if f > s[c]: # frequency in t is greater that frequency in s
+                    return False
+            return True
+
+        freq_count_t = Counter(list(t))
+        min_len = float("inf")
+        min_str = ""
+        for i in range(len(s)):
+            start_id = i+len(t)
+            for j in range(start_id, len(s)+1): # j means the ending index not including the last char.
+                if j == start_id:
+                    freq_count_cur_s = Counter(list(s[i:j]))
+                else:
+                    freq_count_cur_s[s[j-1]] += 1                    
+                if s_contains_t(freq_count_cur_s, freq_count_t):
+                    if min_len > (j-i):
+                        min_str = s[i:j]
+                        min_len = (j-i)
+                    break
+        
+        return min_str
+
