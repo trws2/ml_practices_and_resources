c1bef9e7f0fde8a5c1a5a4874b476ef3a98bf35b
trws2 <dchen@cs.umd.edu>
2025-04-10 23:04:26 +0800

    update

---
+++ day45/1_two_sum.py
@@ -0,0 +1,38 @@
+# runtime: O(nlog(n))
+# space: O(n)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        def twoSumSorted(nums_sorted: List[int]) -> List[int]:
+            left = 0
+            right = len(nums_sorted) - 1
+
+            while True:
+                val = nums_sorted[left] + nums_sorted[right]
+                if val == target:
+                    return [nums_sorted[left], nums_sorted[right]]
+                
+                if val < target:
+                    left += 1
+                    continue
+                
+                if val > target:
+                    right -= 1
+
+        nums_sorted = sorted(nums)
+        left_num, right_num = twoSumSorted(nums_sorted)
+
+        for i, n in enumerate(nums):
+            if left_num == n:
+                left_index = i
+                break
+            
+        for i, n in enumerate(nums):
+            if left_index == i:
+                continue
+            if right_num == n:
+                right_index = i
+                break
+        
+        return [left_index, right_index]
+
