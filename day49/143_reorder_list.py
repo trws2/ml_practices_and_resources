de80454465e24c1dfee9beda41a76d30c16f4bb4
trws2 <dchen@cs.umd.edu>
2025-04-21 12:12:10 -0700

    update

---
+++ day49/143_reorder_list.py
@@ -0,0 +1,41 @@
+# runtime: O(n)
+# space: O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        fast = head
+        slow = head
+
+        # find the middle of the list
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        # reverse the second half of the list
+        second = slow.next
+        slow.next = None
+        node = None
+
+        while second:
+            temp = second.next
+            second.next = node
+            node = second
+            second = temp
+
+        # merge the two halves
+        first = head
+        second = node
+
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next, second.next = second, temp1
+            first, second = temp1, temp2
+
