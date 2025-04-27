dc12b5ae2bfad76407bad09f4c101c3644389ce7
trws2 <dchen@cs.umd.edu>
2025-04-27 14:15:18 -0700

    update

---
+++ day51/253_meeting_rooms_ii.py
@@ -0,0 +1,24 @@
+# runtime: O(n x log(n))
+# space: O(n)
+
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+import heapq
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        intervals.sort(key=lambda x: x.start)
+        min_heap = []
+
+        for interval in intervals:
+            if min_heap and min_heap[0] <= interval.start:
+                heapq.heappop(min_heap)
+            heapq.heappush(min_heap, interval.end)
+
+        return len(min_heap)
+
