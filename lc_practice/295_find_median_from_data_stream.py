fb00490296c2e4ed87425e49d1e2a086224e6b02
trws2 <dchen@cs.umd.edu>
2025-09-13 16:03:43 -0700

    update lc_practice/295_find_median_from_data_stream.py

--- lc_practice/295_find_median_from_data_stream.py
+++ lc_practice/295_find_median_from_data_stream.py
@@ -27,3 +27,44 @@ class MedianFinder:
 # obj = MedianFinder()
 # obj.addNum(num)
 # param_2 = obj.findMedian()
+
+
+# new practice
+
+import heapq
+
+class MedianFinder:
+
+    def __init__(self):
+        self.left = []
+        self.right = []
+
+    def addNum(self, num: int) -> None:
+        # push into the right queue
+        if len(self.right) == 0 or num <= self.right[0]:
+            heapq.heappush(self.left, -num)
+        else:
+            heapq.heappush(self.right, num)
+
+        #
+        if len(self.left) > len(self.right) + 1:
+            curr = -heapq.heappop(self.left)
+            heapq.heappush(self.right, curr)
+        elif len(self.right) > len(self.left):
+            curr = heapq.heappop(self.right)
+            heapq.heappush(self.left, -curr)
+
+    def findMedian(self) -> float:
+        if len(self.left) == len(self.right):
+            ret = (-self.left[0] + self.right[0]) / 2
+        else:
+            ret = -self.left[0]
+        return ret
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
+
+
