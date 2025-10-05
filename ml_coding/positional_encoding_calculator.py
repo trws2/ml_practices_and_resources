75fdf2f34247180828d4200716726ba4af887cd5
trws2 <dchen@cs.umd.edu>
2025-10-05 14:45:20 -0700

    add decision_tree_learning.py and update positional_encoding_calculator.py

--- ml_coding/positional_encoding_calculator.py
+++ ml_coding/positional_encoding_calculator.py
@@ -1,5 +1,30 @@
 # source: https://www.deep-ml.com/problems/85
 
+# newer implementation
+
+import numpy as np
+
+def pos_encoding(position: int, d_model: int):
+    pos_encoding = np.zeros((position, d_model))
+
+    for pos in range(position):
+        even = np.array([float(x) for x in range(0, d_model, 2)])
+        even /= d_model
+        even = np.power(10000, even)
+        val = pos / even
+
+        encoding = np.zeros(d_model)
+        encoding[::2] = np.sin(val)
+        encoding[1::2] = np.cos(val)
+        pos_encoding[pos] = encoding
+
+	return np.float16(pos_encoding)
+
+
+
+# older implementation
+
+
 import numpy as np
 
 def pos_encoding(position: int, d_model: int):
@@ -9,14 +34,14 @@ def pos_encoding(position: int, d_model: int):
         pos_encoding = np.zeros(d_model)
         even = np.array([x for x in range(0, d_model, 2)])
         even = even / d_model
-        odd = np.array([x for x in range(1, d_model, 2)])
-        odd = odd / d_model
+
         pos_encoding[::2] = np.sin(pos / np.power(n, even))
         pos_encoding[1::2] = np.cos(pos / np.power(n, even))
+
         final_pos_encoding[pos] = pos_encoding
 
-    ret = np.float16(final_pos_encoding)
-    return ret
+	ret = np.float16(final_pos_encoding)
+	return ret
 
 if __name__ == "__main__":
     print(pos_encoding(2, 8))
