43b4f6360cd3202ea04af6a5580602144c39a625
trws2 <dchen@cs.umd.edu>
2025-10-12 23:22:25 -0700

    update ml_coding/implement_layer_normalization_for_sequence_data.py

--- ml_coding/implement_layer_normalization_for_sequence_data.py
+++ ml_coding/implement_layer_normalization_for_sequence_data.py
@@ -4,8 +4,19 @@ def layer_normalization(X: np.ndarray, gamma: np.ndarray, beta: np.ndarray, epsi
 	"""
 	Perform Layer Normalization.
 	"""
-    m = np.mean(X, axis=(2), keepdims=True)
-    s = np.std(X, axis=(2), keepdims=True)    
-    val = (X - m) / s 
+    m = np.mean(X, axis=-1, keepdims=True)
+    v = np.var(X, axis=-1, keepdims=True)
+    val = (X - m) / np.sqrt(v + 1e-6)
     return gamma * val + beta
+
+
+def batch_normalization(X: np.ndarray, gamma: np.ndarray, beta: np.ndarray, epsilon: float = 1e-5) -> np.ndarray:
+	"""
+	Perform Layer Normalization.
+	"""
+    m = np.mean(X, axis=(1, 2), keepdims=True)
+    v = np.var(X, axis=(1, 2), keepdims=True)
+    val = (X - m) / np.sqrt(v + 1e-6)
+    return gamma * val + beta
+
 
